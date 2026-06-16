f7d54fcdfb5c47f48ef980c9613b59efd3dd6b68
CodeHermit17 <kashyap17.vaghasiya@gmail.com>
2026-06-16 17:45:42 +0530

    FIX: changed relative number colors

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,5 +1,8 @@
 vim.opt.number = true
 vim.opt.cursorline = true
-vim.opt.relativenumber =true
+vim.opt.relativenumber = true
 vim.opt.shiftwidth = 4
 
+vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "#9fa8da" })
+vim.api.nvim_set_hl(0, "LineNr", { fg = "#ffffff" }) -- current line number
+vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "#9fa8da" })
